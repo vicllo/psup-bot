ea33f57b9182f271bdf512564b18397e54657460
vicllo <vicllo@users.noreply.github.com>
2021-05-27 12:39:49 +0200

    Add the multiple inputs for add_event

--- constants/constants.py
+++ constants/constants.py
@@ -5,3 +5,18 @@ all_event_kinds = {"Accepted": AcceptEvent,
                    "SchoolRefused": SchoolRefuseEvent,
                    "Waiting": WaitingListEvent,
                    "Proposition": PropositionEvent}
+
+reactions_order = {0: "0️⃣",
+                   1: "1️⃣",
+                   2: "2️⃣",
+                   3: "3️⃣",
+                   4: "4️⃣",
+                   5: "5️⃣",
+                   6: "6️⃣",
+                   7: "7️⃣",
+                   8: "8️⃣",
+                   9: "9️⃣",
+                   "previous": "◀",
+                   "next": "▶",
+                   "close": "❌"
+                   }
--- main.py
+++ main.py
@@ -64,6 +64,113 @@ def get_plot(user_id, courses):
     return plt_file_name
 
 
+async def get_course(ctx, session, page=0):
+    user_id = ctx.author.id
+    embed = discord.Embed(title="Chose your course")
+    courses_names_list = list(session.courses.keys())
+    choice_emojies = {}
+    for index in range(min(len(courses_names_list), 10*(page+1))-10*page):
+        embed.add_field(name=reactions_order[index], value=courses_names_list[9*page+index], inline=False)
+        choice_emojies[reactions_order[index]] = index
+    send_embed = await ctx.send(embed=embed)
+
+    for emoji in choice_emojies.keys():
+        await send_embed.add_reaction(emoji)
+    if page > 0:
+        await send_embed.add_reaction(reactions_order["previous"])
+        choice_emojies[reactions_order["previous"]] = "previous"
+    if page < (len(courses_names_list)-1)//10:
+        await send_embed.add_reaction(reactions_order["next"])
+        choice_emojies[reactions_order["next"]] = "next"
+    await send_embed.add_reaction(reactions_order["close"])
+    choice_emojies[reactions_order["close"]] = "close"
+
+    def check(reaction, user):
+        return user.id == user_id and reaction.emoji in choice_emojies
+
+    reaction, user = await bot.wait_for('reaction_add', check=check)
+    command = choice_emojies[reaction.emoji]
+
+    if type(command) == int:
+        return courses_names_list[9*page+command]
+    elif command == "next":
+        return await get_course(ctx, session, page + 1)
+    elif command == "previous":
+        return await get_course(ctx, session, page - 1)
+    elif command == "close":
+        return None
+    else:
+        raise ValueError('Unknown command after embed course choice')
+
+
+async def get_date(ctx):
+    def check(m):
+        return m.author.id == user_id
+    user_id = ctx.author.id
+
+    await ctx.channel.send("Send the date. Use the format like this : 27-05-2021")
+    date_message = await bot.wait_for('message', check=check)
+    str_date = date_message.content
+    try:
+        day, month, year = map(int,str_date.split("-"))
+    except:
+        await ctx.channel.send("Wrong date format")
+        return await get_date(ctx)
+
+    await ctx.channel.send("Send the hour. Use the format like this : 18:30:00")
+    hour_message = await bot.wait_for('message', check=check)
+    str_hour = hour_message.content
+    try:
+        hour, minute, second = map(int,str_hour.split(":"))
+    except:
+        await ctx.channel.send("Wrong hour format")
+        return await get_date(ctx)
+    try:
+        date = datetime(year, month, day, hour, minute, second)
+    except:
+        await ctx.channel.send("Wrong datetime format")
+        return await get_date(ctx)
+
+    return date
+
+
+async def get_type(ctx):
+    user_id = ctx.author.id
+    embed = discord.Embed(title="Chose your course")
+    event_type_list = list(all_event_kinds.keys())
+    choice_emojies = {}
+    for index in range(len(all_event_kinds)):
+        embed.add_field(name=reactions_order[index], value=event_type_list[index], inline=False)
+        choice_emojies[reactions_order[index]] = index
+    send_embed = await ctx.send(embed=embed)
+
+    for emoji in choice_emojies.keys():
+        await send_embed.add_reaction(emoji)
+
+    def check(reaction, user):
+        return user.id == user_id and reaction.emoji in choice_emojies
+
+    reaction, user = await bot.wait_for('reaction_add', check=check)
+    command = choice_emojies[reaction.emoji]
+    return all_event_kinds[event_type_list[command]]
+
+
+async def get_place(ctx):
+    def check(m):
+        return m.author.id == user_id
+
+    user_id = ctx.author.id
+
+    await ctx.channel.send("What is your actual rank ?")
+    rank_message = await bot.wait_for('message', check=check)
+    try:
+        rank = int(rank_message.content)
+    except:
+        await ctx.channel.send("Please send an integer")
+        return await get_place(ctx)
+    return rank
+
+
 # detecter l'allumage du bot
 @bot.event
 async def on_ready():
@@ -148,37 +255,66 @@ async def add_course(ctx, course_name, places_available, previous_last_entry):
 
 
 @bot.command()
-async def add_event(ctx, course_name, date, event_type, place=-1):
+async def add_event(ctx, course_name=None, date=None, event_kind=None, place=None):
     """
-    Command to add a new event. Enter the course name related to the event, the date (ISO format), the event kind and, if necessary, the place related.
+    Command to add a new event.
+    Enter the course name, the date (ISO format), the event kind and if necessary, the place related.
     Possible events : Accepted, UserRefused, SchoolRefused, Waiting, Proposition
     Date format : YYYY-MM-DDTHH:MM:SS
     Ex : .add_event Orsay_MI 2021-06-27T19:30:00 Waiting 285
+    Ex : .add_event
     """
-    # TODO : Split the entries in separate messages
     user_id = ctx.author.id
     user_session = get_session(user_id)
-    try:
+
+    # GET THE COURSE NAME
+    if course_name in user_session.courses:
         course = user_session.courses[course_name]
-    except KeyError:
-        await ctx.channel.send("Course not found. Please add the course")
-        raise KeyError
-    try:
-        date = datetime.fromisoformat(date)
-    except ValueError:
-        await ctx.channel.send("Invalid format string. Use the ISO format YYYY-MM-DDTHH:MM:SS")
-        raise ValueError
-    place = int(place)
-    if event_type == "Waiting":
+    else:
+        course_name = await get_course(ctx, user_session)
+        if not course_name:
+            await ctx.channel.send("Command canceled")
+            return -1
+        course = user_session.courses[course_name]
+
+    # GET THE DATE
+    if date:
+        try:
+            date = datetime.fromisoformat(date)
+        except ValueError:
+            await ctx.channel.send("Invalid format string. Next time use the ISO format YYYY-MM-DDTHH:MM:SS. "
+                                   "Now let's enter the date manually")
+            date = await get_date(ctx)
+
+    else:
+        date = await get_date(ctx)
+
+    # GET THE EVENT TYPE
+    if event_kind:
+        event_type = all_event_kinds[event_kind]
+    else:
+        event_type = await get_type(ctx)
+
+    # CREATE THE EVENT
+    if event_type == WaitingListEvent:
+        if place:
+            try:
+                place = int(place)
+            except:
+                await ctx.channel.send("Please send an integer")
+                place = await get_place(ctx)
+        else:
+            place = await get_place(ctx)
+
         new_event = WaitingListEvent(date, course, place)
         course.add_event(new_event)
     else:
-        new_event = all_event_kinds[event_type](date, course)
+        new_event = event_type(date, course)
         course.add_event(new_event)
 
     with open("datas/"+str(user_id)+"/events.csv", 'a') as events_file:
         events_file.write(str(new_event)+"\n")
-    await ctx.channel.send("Le nouvel évènement a été ajouté avec succès")
+    await ctx.channel.send("The new event was successfully added")
 
 
 @bot.command()
@@ -210,7 +346,7 @@ async def plot(ctx, *courses):
             if courses[i] in user_session.courses:
                 courses[i] = user_session.courses[courses[i]]
             else:
-                await ctx.channel.send(courses[i]+" n'a pas été trouvé")
+                await ctx.channel.send(courses[i]+" has not been found")
                 courses[i] = None
     plot_file_name = get_plot(user_id, courses)
 
@@ -221,4 +357,3 @@ load_dotenv()
 token = os.getenv('PSUP-BOT-TOKEN')
 
 bot.run(token)
-
